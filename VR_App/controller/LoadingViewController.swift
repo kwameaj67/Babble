7e531cfe21c67e2facc4d82b877fa9f8236c75a1
Kwame <kwameagyenimboateng64@gmail.com>
2021-08-04 14:12:14 +0000

    🚧 modified loading screen

--- VR_App/controller/LoadingViewController.swift
+++ VR_App/controller/LoadingViewController.swift
@@ -12,30 +12,29 @@ class LoadingViewController: UIViewController {
     private let isLoggedIn = true
     override func viewDidLoad() {
         super.viewDidLoad()
-        setupViews()
     }
     
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
-        DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) {
+        
+        delay(duration: 2.0) {
             self.showInitialView()
         }
-            }
-    
-    private func setupViews(){
-        view.backgroundColor = .systemGray
     }
+    
+  
     
     private func showInitialView(){
 //  if user is logged in, shows tabBar controller
 //  if user is NOT logged in, show onboarding flow
         if isLoggedIn {
-            let tabBarController = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(identifier: "MainTabController")
-            if let sceneDelegate = view.window?.windowScene?.delegate as? SceneDelegate, let window = sceneDelegate.window{
-                window.rootViewController = tabBarController
-            }
+            PresenterManager.shared.showViewController(vc: .onboarding)
+//            let tabBarController = UIStoryboard(name:Constants.StoryboardID.mainStoryboard, bundle: nil).instantiateViewController (identifier: Constants.StoryboardID.tabBarController)
+//            if let sceneDelegate = view.window?.windowScene?.delegate as? SceneDelegate, let window = sceneDelegate.window{
+//                window.rootViewController = tabBarController
+//            }
         }else{
-            performSegue(withIdentifier: "showOnboarding", sender: nil)
+            performSegue(withIdentifier: Constants.Segue.showOnboarding , sender: nil)
         }
     }
 
