d5d5a70e25f5abae17c2fa879bab28a7f8bd4fc5
Kwame <kwameagyenimboateng64@gmail.com>
2021-08-04 02:52:52 +0000

    🚧add constants

---
+++ VR_App/helper/constants.swift
@@ -0,0 +1,20 @@
+//
+//  constants.swift
+//  VR_App
+//
+//  Created by Kwame Agyenim - Boateng on 04/08/2021.
+//
+
+import Foundation
+
+
+struct Constants {
+    struct Segue {
+        static let showOnboarding = "showOnboarding"
+    }
+    
+    struct StoryboardID{
+        static let mainStoryboard = "Main"
+        static let tabBarController = "MainTabController"
+    }
+}
