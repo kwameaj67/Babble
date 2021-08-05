3bdb02fa63bf653cba5c6f93d40bc90a5ece7065
Kwame <kwameagyenimboateng64@gmail.com>
2021-08-05 15:17:03 +0000

    🥳 modified loading screen

--- VR_App/controller/Auth/LoadingViewController.swift
+++ VR_App/controller/Auth/LoadingViewController.swift
@@ -12,6 +12,7 @@ class LoadingViewController: UIViewController {
     private let isLoggedIn = true
     override func viewDidLoad() {
         super.viewDidLoad()
+    
     }
     
     override func viewDidAppear(_ animated: Bool) {
@@ -28,7 +29,7 @@ class LoadingViewController: UIViewController {
 //  if user is logged in, shows tabBar controller
 //  if user is NOT logged in, show onboarding flow
         if isLoggedIn {
-            PresenterManager.shared.showViewController(vc: .onboarding)
+            PresenterManager.shared.showViewController(vc: .mainTabController)
 //            let tabBarController = UIStoryboard(name:Constants.StoryboardID.mainStoryboard, bundle: nil).instantiateViewController (identifier: Constants.StoryboardID.tabBarController)
 //            if let sceneDelegate = view.window?.windowScene?.delegate as? SceneDelegate, let window = sceneDelegate.window{
 //                window.rootViewController = tabBarController
@@ -37,6 +38,8 @@ class LoadingViewController: UIViewController {
             performSegue(withIdentifier: Constants.Segue.showOnboarding , sender: nil)
         }
     }
+    
+   
 
 }
 
