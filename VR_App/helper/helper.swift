44355a0ee7ef55256e13300680279c807d035c0a
Kwame <kwameagyenimboateng64@gmail.com>
2021-08-10 00:13:00 +0000

    added roundCorners

--- VR_App/helper/helper.swift
+++ VR_App/helper/helper.swift
@@ -6,9 +6,15 @@
 //
 
 import Foundation
+import UIKit
 
 
 
 func delay(duration: Double, completion: @escaping () -> Void){
     DispatchQueue.main.asyncAfter(deadline: .now() + duration, execute: completion)
+}
+
+
+func roundCorners(button:UIButton){
+    button.layer.cornerRadius = button.layer.frame.height / 2
 }
