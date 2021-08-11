8c369b64babd281989085862a203b5d039569272
Kwame <kwameagyenimboateng64@gmail.com>
2021-08-11 16:43:34 +0000

    📍created pulse animation to buttons

--- VR_App/helper/helper.swift
+++ VR_App/helper/helper.swift
@@ -18,3 +18,14 @@ func delay(duration: Double, completion: @escaping () -> Void){
 func roundCorners(button:UIButton){
     button.layer.cornerRadius = button.layer.frame.height / 2
 }
+
+ func animatePulseButton(_ animateTo:UIView){
+    UIView.animate(withDuration: 0.3, delay: 0, usingSpringWithDamping: 2, initialSpringVelocity: 1.5, options: .curveEaseIn) {
+        animateTo.transform = CGAffineTransform(scaleX: 0.93, y:0.93)
+    } completion: { (_) in
+        UIView.animate(withDuration: 0.3, delay: 0, usingSpringWithDamping: 1, initialSpringVelocity: 1, options: .curveEaseIn, animations: {
+            animateTo.transform = CGAffineTransform(scaleX: 1, y: 1)
+        }, completion:nil)
+        print("Animation complete")
+    }
+}
