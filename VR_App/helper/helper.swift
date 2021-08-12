4d7ffa2dff24c37bbdd37444c68e3510e4807b2e
Kwame <kwameagyenimboateng64@gmail.com>
2021-08-12 15:34:55 +0000

    add more helper functions

--- VR_App/helper/helper.swift
+++ VR_App/helper/helper.swift
@@ -18,7 +18,9 @@ func delay(duration: Double, completion: @escaping () -> Void){
 func roundCorners(button:UIButton){
     button.layer.cornerRadius = button.layer.frame.height / 2
 }
-
+func textInputRoundCorners(view:UIView){
+    view.layer.cornerRadius = view.layer.frame.height / 2
+}
  func animatePulseButton(_ animateTo:UIView){
     UIView.animate(withDuration: 0.3, delay: 0, usingSpringWithDamping: 2, initialSpringVelocity: 1.5, options: .curveEaseIn) {
         animateTo.transform = CGAffineTransform(scaleX: 0.93, y:0.93)
@@ -29,3 +31,8 @@ func roundCorners(button:UIButton){
         print("Animation complete")
     }
 }
+func inputConfig(input:UITextField){
+    input.autocorrectionType = .no
+    input.autocapitalizationType = .none
+}
+// CGColor(red: 0.26, green: 0.52, blue: 0.54, alpha: 1.00) 
