20b29d2081e30505d096951c5e4a51b70292a901
Kwame <kwameagyenimboateng64@gmail.com>
2021-08-13 14:52:13 +0000

    🚧create User model

---
+++ VR_App/model/Main/User.swift
@@ -0,0 +1,21 @@
+//
+//  User.swift
+//  VR_App
+//
+//  Created by Kwame Agyenim - Boateng on 13/08/2021.
+//
+
+import Foundation
+
+struct User {
+    var name: String?
+    var email:String?
+    var password:String?
+    var gender:String?
+    var identity:String?
+    var transcripts:[Transcript]
+    
+
+}
+
+
