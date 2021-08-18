4f726934efec8c93fb256580275ebfb87342cf1f
Kwame <kwameagyenimboateng64@gmail.com>
2021-08-18 12:51:47 +0000

    🚧 add searchBar

--- VR_App/controller/Main/TranscriptionsViewController.swift
+++ VR_App/controller/Main/TranscriptionsViewController.swift
@@ -8,22 +8,18 @@
 import UIKit
 
 class TranscriptionsViewController: UIViewController {
-
+    
+    let searchBar = UISearchController()
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        navigationItem.searchController = searchBar
+        
+        let searchTextAppearance = UITextField.appearance(whenContainedInInstancesOf: [UISearchBar.self])
+        searchTextAppearance.font = UIFont(name: "Avenir", size: 14)
+        searchTextAppearance.textColor = .black
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
