3ad9504663bcb381d859887019a78877b76c4736
Kwame <kwameagyenimboateng64@gmail.com>
2021-08-19 17:03:32 +0000

    home changes

--- VR_App/controller/Main/HomeViewController.swift
+++ VR_App/controller/Main/HomeViewController.swift
@@ -6,13 +6,18 @@
 //
 
 import UIKit
+import Firebase
 
 class HomeViewController: UIViewController {
-
+   
+    @IBOutlet weak var recordButton: UIButton!
+    @IBOutlet weak var recordImage: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        styles()
+        animation(recordButton)
+        let user = Auth.auth().currentUser?.email
+        print(user ?? "user no found")
     }
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
@@ -22,5 +27,27 @@ class HomeViewController: UIViewController {
         super.viewWillDisappear(animated)
         navigationController?.setNavigationBarHidden(false, animated: animated)
     }
+    func styles(){
+        roundCorners(button: recordButton)
+        recordButton.layer.shadowColor = Constants.Colors.CGgreen
+        recordButton.layer.shadowRadius = 35
+        recordButton.layer.shadowOpacity = 1
+        recordButton.layer.shadowOffset = CGSize(width: 0, height: 0)
+    }
+    func animation(_ animateTo: UIButton){
+        UIButton.animate(withDuration: 1.0, delay: 0, usingSpringWithDamping: 1.5, initialSpringVelocity: 0.0, options: [.autoreverse,.repeat,.curveEaseIn]) {
+            animateTo.transform = CGAffineTransform(scaleX: 1, y: 1)
+        } completion: { (_) in
+            UIButton.animate(withDuration: 1.0, delay: 0, usingSpringWithDamping: 1.0, initialSpringVelocity: 0.0, options: [.repeat, .autoreverse, .curveEaseIn], animations: {
+                animateTo.transform = CGAffineTransform(scaleX: 0.95, y: 0.95)
+                
+            }, completion:nil)
+        }
+    }
 
+  
+    @IBAction func onTapRecordBustton(_ sender: UIButton) {
+        print("Tapped")
+        
+    }
 }
