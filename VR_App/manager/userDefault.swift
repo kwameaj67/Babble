a2554c065d796a4a9d8240c6805fdde997847f28
Kwame <kwameagyenimboateng64@gmail.com>
2021-08-21 03:13:43 +0000

    create userDefault struct

---
+++ VR_App/manager/userDefault.swift
@@ -0,0 +1,14 @@
+//
+//  userDefault.swift
+//  VR_App
+//
+//  Created by Kwame Agyenim - Boateng on 20/08/2021.
+//
+
+import Foundation
+import UIKit
+
+struct userDefaultManager {
+    static let userDefault = UserDefaults.standard
+    
+}
