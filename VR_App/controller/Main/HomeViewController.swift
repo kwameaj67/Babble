369d6eb4df2d2df05a2c8a93a3f5fcecdc02d8a5
Kwame <kwameagyenimboateng64@gmail.com>
2021-08-21 03:15:45 +0000

    modified HomeVC with userDefaults

--- VR_App/controller/Main/HomeViewController.swift
+++ VR_App/controller/Main/HomeViewController.swift
@@ -10,17 +10,22 @@ import Firebase
 
 class HomeViewController: UIViewController {
    
+    @IBOutlet weak var nameTextLabel: UILabel!
+    @IBOutlet weak var avatarImage: UIImageView!
     @IBOutlet weak var recordButton: UIButton!
-    @IBOutlet weak var recordImage: UIImageView!
+    @IBOutlet weak var captionTitleLabel: UILabel!
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
-        
+        animation(recordButton)
+        recordButton.isSelected = false
+        delay(duration: 3.0) {
+            self.captionTitleLabel.text = "Record your speech"
+        }
     }
     override func viewDidLoad() {
         super.viewDidLoad()
         styles()
-        animation(recordButton)
-        recordButton.isUserInteractionEnabled = true
+        getUserDefaultsData()
         let user = Auth.auth().currentUser?.email
         print(user ?? "user no found")
     }
@@ -33,6 +38,26 @@ class HomeViewController: UIViewController {
         navigationController?.setNavigationBarHidden(false, animated: animated)
     }
     
+    
+  
+    @IBAction func onTapRecordBustton(_ sender: UIButton) {
+        print("Tapped")
+        recordButton.isSelected = !recordButton.isSelected
+//        animatePulseButton(recordButton)
+        if recordButton.isSelected == true{
+            captionTitleLabel.text = "Now listening for sound to captions"
+        }else{
+            captionTitleLabel.text = "Tap to record captions"
+        }
+        let vc = storyboard?.instantiateViewController(identifier: Constants.StoryboardID.errorViewController) as! ErrorViewController
+        present(vc, animated: true, completion: nil)
+        modalPresentationStyle = .fullScreen
+        
+    }
+}
+
+extension HomeViewController{
+//    MARK: - Styles
     func styles(){
         roundCorners(button: recordButton)
         recordButton.layer.shadowColor = Constants.Colors.CGgreen
@@ -40,15 +65,35 @@ class HomeViewController: UIViewController {
         recordButton.layer.shadowOpacity = 1
         recordButton.layer.shadowOffset = CGSize(width: 0, height: 0)
     }
+//    MARK: - animate button
     func animation(_ animateTo: UIView){
-        UIView.animate(withDuration: 1.3, delay: 0, usingSpringWithDamping: 1.5, initialSpringVelocity: 0.0, options: [.repeat,.autoreverse,.curveLinear,.allowUserInteraction,], animations: {
+        UIView.animate(withDuration: 1.5, delay: 0, usingSpringWithDamping: 0.9, initialSpringVelocity: 0.0, options: [.repeat,.autoreverse,.curveLinear,.allowUserInteraction,], animations: {
             animateTo.transform = CGAffineTransform(scaleX: 0.96, y: 0.96)
         }, completion: nil)
-     
     }
-  
-    @IBAction func onTapRecordBustton(_ sender: UIButton) {
-        print("Tapped")
-//        animation(recordButton)
+//    MARK: - read userDefaults
+    func getUserDefaultsData(){
+        // used to format name strings
+        let nameFormatter = PersonNameComponentsFormatter()
+       
+        // read data from userDefaults
+        let userData = userDefaultManager.userDefault.object(forKey: "user") as? [String:String]
+        
+        for data in userData ?? [:]{
+            print("\(data)")
+        }
+        let name:String = userData?["username"] ?? ""
+        let gender:String = userData?["gender"] ?? ""
+        if let userName = nameFormatter.personNameComponents(from: name){
+            nameFormatter.style = .short
+            nameTextLabel.text =  "Hey, \(nameFormatter.string(from: userName)) 👋"
+        }
+        // checks gender type
+        if gender == "Male"{
+            avatarImage.image = UIImage(named: "icons8-man-medium-skin-tone-100")
+        }else if gender == "Female"{
+            avatarImage.image = UIImage(named: "icons8-woman-curly-hair-medium-skin-tone-100")
+        }
+        
     }
 }
