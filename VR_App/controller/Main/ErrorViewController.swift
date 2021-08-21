a2e6af88ac48004dfda2c6bf3fab064d2bcbd4c5
Kwame <kwameagyenimboateng64@gmail.com>
2021-08-21 03:13:08 +0000

    modified errorVC

--- VR_App/controller/Main/ErrorViewController.swift
+++ VR_App/controller/Main/ErrorViewController.swift
@@ -9,13 +9,26 @@ import UIKit
 
 class ErrorViewController: UIViewController {
 
+    @IBOutlet weak var tryAgainButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        styles()
+       
     }
     
 
-   
+    @IBAction func onTapTryAgain(_ sender: Any) {
+    }
+    
 
+}
+
+extension ErrorViewController{
+    func styles(){
+        roundCorners(button: tryAgainButton)
+        tryAgainButton.layer.shadowColor = Constants.Colors.CGgreen
+        tryAgainButton.layer.shadowRadius = 10
+        tryAgainButton.layer.shadowOpacity = 1
+        tryAgainButton.layer.shadowOffset = CGSize(width: 0, height: 0)
+    }
 }
