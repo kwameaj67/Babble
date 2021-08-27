918428ac184768c25b184b16e040734534cf54cc
Kwame <kwameagyenimboateng64@gmail.com>
2021-08-27 19:26:25 +0000

    added bridging headers

---
+++ sources/Bridging-Header.h
@@ -0,0 +1,16 @@
+//
+//  Bridging-Header.h
+//  VR_App
+//
+//  Created by Kwame Agyenim - Boateng on 26/08/2021.
+//
+
+#ifndef Bridging_Header_h
+#define Bridging_Header_h
+
+//#import "googleapis/CloudSpeech.pbobjc.h"
+//#import "googleapis/CloudSpeech.pbrpc.h"
+#import "google/cloud/speech/v1/CloudSpeech.pbobjc.h"
+#import "google/cloud/speech/v1/CloudSpeech.pbrpc.h"
+
+#endif /* Bridging_Header_h */
