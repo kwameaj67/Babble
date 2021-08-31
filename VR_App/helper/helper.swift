8c923548d298494862cc6ce67ba4811cf14d9147
Kwame <kwameagyenimboateng64@gmail.com>
2021-08-31 15:20:54 +0000

    extra changes

--- VR_App/helper/helper.swift
+++ VR_App/helper/helper.swift
@@ -8,19 +8,19 @@
 import Foundation
 import UIKit
 
-
-
+// MARK: - delay
 func delay(duration: Double, completion: @escaping () -> Void){
     DispatchQueue.main.asyncAfter(deadline: .now() + duration, execute: completion)
 }
 
-
+// MARK:- rounded corners
 func roundCorners(button:UIButton){
     button.layer.cornerRadius = button.layer.frame.height / 2
 }
 func textInputRoundCorners(view:UIView){
     view.layer.cornerRadius = view.layer.frame.height / 2
 }
+// MARK:- pulse button animation
  func animatePulseButton(_ animateTo:UIView){
     UIView.animate(withDuration: 0.3, delay: 0, usingSpringWithDamping: 2, initialSpringVelocity: 1.5, options: .curveEaseIn) {
         animateTo.transform = CGAffineTransform(scaleX: 0.93, y:0.93)
@@ -30,11 +30,12 @@ func textInputRoundCorners(view:UIView){
         }, completion:nil)
     }
 }
+// MARK: - textFields configs
 func inputConfig(input:UITextField){
     input.autocorrectionType = .no
     input.autocapitalizationType = .none
 }
 
-// referencing the managed object context
+// MARK: -  referencing the managed object context
  let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
 
