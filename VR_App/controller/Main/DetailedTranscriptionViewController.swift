00ae7d948c9ada6c04418c248945a3c69ce07cfc
Kwame <kwameagyenimboateng64@gmail.com>
2021-09-08 16:55:28 +0000

    created detailedTranscripts VC

---
+++ VR_App/controller/Main/DetailedTranscriptionViewController.swift
@@ -0,0 +1,23 @@
+//
+//  DetailedTranscriptionViewController.swift
+//  VR_App
+//
+//  Created by Kwame Agyenim - Boateng on 08/09/2021.
+//
+
+import UIKit
+
+class DetailedTranscriptionViewController: UIViewController {
+
+    var noteDate: String = ""
+    var noteTitle:String = ""
+    var noteDescription:String = ""
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+       
+    }
+    
+
+
+}
