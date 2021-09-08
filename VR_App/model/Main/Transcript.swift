0347c63b6314f104acce3cec6c8bcec3f4410bbf
Kwame <kwameagyenimboateng64@gmail.com>
2021-09-08 16:56:03 +0000

    updated model

--- VR_App/model/Main/Transcript.swift
+++ VR_App/model/Main/Transcript.swift
@@ -6,6 +6,7 @@
 //
 
 import Foundation
+import UIKit
 //
 //{
 //  "name": "1268386125834704889",
@@ -26,7 +27,21 @@ import Foundation
 //  "done": True,
 //}
 
-
+struct TranscriptModel{
+    var title: String
+    var description:String
+    var date:String
+    
+    static let transcriptData: [TranscriptModel] = [
+        TranscriptModel(title: "Beats", description: "Album 150 130Bmp", date:"05/05/2021"),
+        TranscriptModel(title: "Web application tools", description: "I would be a first billionaire", date:"05/05/2021"),
+        TranscriptModel(title: "ASR technology", description: "Automated speech recognition is any technology that allows a computer to convert spoken language into typed text in real time", date:"05/05/2021"),
+        TranscriptModel(title: "Voice recognition App", description: "Wise Quotes Before you perish, it's significant for your hereafter to leave behind or to give to the world your wisdom, experience, and knowledge, for humanity thrives on the past, present and to forecast the future.", date:"05/05/2021"),
+        TranscriptModel(title: "Personal timetable", description: "This contains the semester timetable according to my courses", date:"05/05/2021"),
+        TranscriptModel(title: "📌IOS Articles", description: "ios Development https://sarunw.com/posts/", date:"05/05/2021"),
+    ]
+    
+}
 struct ResponseData: Decodable{
     var data:Response
     var done: Bool
