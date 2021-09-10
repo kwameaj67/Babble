8d3b60b3b5906c072237992c1fd52b9703242254
Kwame <kwameagyenimboateng64@gmail.com>
2021-09-10 04:05:27 +0000

    detailedVC info

--- VR_App/controller/Main/DetailedTranscriptionViewController.swift
+++ VR_App/controller/Main/DetailedTranscriptionViewController.swift
@@ -9,15 +9,18 @@ import UIKit
 
 class DetailedTranscriptionViewController: UIViewController {
 
+    @IBOutlet var dateLabel: UILabel!
+    @IBOutlet var titleTextField: UITextField!
+    @IBOutlet var descriptionTextArea: UITextView!
     var noteDate: String = ""
     var noteTitle:String = ""
     var noteDescription:String = ""
     override func viewDidLoad() {
         super.viewDidLoad()
-
-       
+        title = "Files"
+        dateLabel.text = noteDate
+        titleTextField.text = noteTitle
+        descriptionTextArea.text = noteDescription
     }
-    
-
 
 }
