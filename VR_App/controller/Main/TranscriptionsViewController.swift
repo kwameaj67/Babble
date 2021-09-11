4d7265fdd3b659c38c350774a580e70f7b4deebf
Kwame <kwameagyenimboateng64@gmail.com>
2021-09-11 00:33:41 +0000

    transcriptionVC changes

--- VR_App/controller/Main/TranscriptionsViewController.swift
+++ VR_App/controller/Main/TranscriptionsViewController.swift
@@ -7,68 +7,102 @@
 
 import UIKit
 
-class TranscriptionsViewController: UIViewController {
+protocol TranscriptionListDelegate{
+    func refreshTranscriptions()
+}
+class TranscriptionsViewController: UIViewController{
     
+    @IBOutlet var iconContainer: UIView!
     @IBOutlet var numOfNotes: UILabel!
     @IBOutlet var emptyMsgLabel: UILabel!
-    var transcriptionArray:[TranscriptModel] = TranscriptModel.transcriptData
-    var myDate:String = ""
-    var myTitle:String = ""
-    var myDescription:String = ""
+//    var transcriptionArray:[TranscriptModel] = TranscriptModel.transcriptData
+    var transcriptionArray:[Transcriptions]? {
+        didSet {
+            numOfNotes.text = "\(String(describing: transcriptionArray?.count ?? 0)) \(transcriptionArray?.count == 1 ? "Note":"Notes")"
+        }
+    }
     @IBOutlet var transcriptionTableVIew: UITableView!
    
-    let searchBar = UISearchController()
+    let searchController = UISearchController()
     override func viewDidLoad() {
         super.viewDidLoad()
         styles()
         transcriptionTableVIew.delegate = self
         transcriptionTableVIew.dataSource = self
         transcriptionTableVIew.layer.cornerRadius = 10
-        navigationItem.searchController = searchBar
+        configureSearchBar()
+        fetchTranscriptions()
     }
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        let destinationVC = segue.destination as! DetailedTranscriptionViewController
-        destinationVC.noteDate = myDate
-        destinationVC.noteDescription = myDescription
-        destinationVC.noteTitle = myTitle
-        
+    private func configureSearchBar(){
+        navigationItem.searchController = searchController
+        searchController.obscuresBackgroundDuringPresentation = false
+        searchController.searchBar.delegate = self
+        searchController.delegate = self
     }
+    
+    //    fetch transcriptions
+        func fetchTranscriptions(){
+            transcriptionArray  = CoreDataManager.shared.fetchTranscriptions()
+            transcriptionArray?.reverse()
+        }
+
 }
 
 extension TranscriptionsViewController:UITableViewDelegate,UITableViewDataSource{
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        numOfNotes.text = transcriptionArray.count > 1 ? "\(transcriptionArray.count) notes" : "\(transcriptionArray.count) note"
-        return transcriptionArray.count
+        return self.transcriptionArray?.count ?? 0
 }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! TranscriptionTableViewCell
         cell.selectionStyle = .none
-        let row = transcriptionArray[indexPath.row]
-        cell.setupTranscriptionCell(item: row)
+        let item = self.transcriptionArray![indexPath.row]
+        cell.setupTranscriptionCell(item: item)
         return cell
     }
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         return 78.0
     }
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        var row = transcriptionArray[indexPath.row]
-//        print("\(row.title)")
-        myTitle = row.title
-        myDescription =  row.description
-        myDate = row.date
-        let data = [
-            "date": myDate,
-            "title": myTitle,
-            "description":myDescription
-        ]
-        print("\(data)")
+        let row = transcriptionArray![indexPath.row]
         
 //        move to new screen
-//        let vc = storyboard?.instantiateViewController(identifier: Constants.StoryboardID.detailedTranscriptViewController) as! DetailedTranscriptionViewController
-//        navigationController?.pushViewController(vc, animated: true)
+        let vc = storyboard?.instantiateViewController(identifier: Constants.StoryboardID.detailedTranscriptViewController) as! DetailedTranscriptionViewController
+        let formatter = DateFormatter()
+        let transcriptDate = formatter.string(from: row.date ?? getDate())
+        vc.noteDate = transcriptDate
+        vc.noteTitle = row.title ?? ""
+        vc.noteDescription = row.text ?? ""
+        navigationController?.pushViewController(vc, animated: true)
     }
-    
+    func tableView(_ tableView: UITableView, trailingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
+        let action = UIContextualAction(style: .destructive, title: "Delete") { action, view, completionHandlder in
+//            get object to remove
+            let transcriptObject = self.transcriptionArray![indexPath.row]
+//            delete object
+            CoreDataManager.shared.deleteTranscription(transcript: transcriptObject)
+            DispatchQueue.main.async {
+                self.transcriptionTableVIew.reloadData()
+            }
+            self.fetchTranscriptions()
+        }
+        return UISwipeActionsConfiguration(actions: [action])
+    }
+//    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+//        let transcriptObject = self.transcriptionArray![indexPath.row]
+//        if editingStyle == .delete{
+//           let delete = CoreDataManager.shared.deleteTranscription(transcript: transcriptObject)
+//            switch delete {
+//            case .success():
+//                DispatchQueue.main.async {
+//                    self.transcriptionTableVIew.reloadData()
+//                }
+//            case .failure(_):
+//                print("cannot delete item")
+//            }
+//
+//        }
+//    }
     
 // MARK:- edits search bar
     func styles(){
@@ -82,6 +116,13 @@ extension TranscriptionsViewController:UITableViewDelegate,UITableViewDataSource
         ]
         UIBarButtonItem.appearance(whenContainedInInstancesOf: [UISearchBar.self]).setTitleTextAttributes(attributes, for: .normal)
     }
+}
 
+extension TranscriptionsViewController: TranscriptionListDelegate{
+    func refreshTranscriptions() {
+        transcriptionTableVIew.reloadData()
+    }
+}
+extension TranscriptionsViewController: UISearchBarDelegate, UISearchControllerDelegate {
     
 }
