fa9c8a1279cf80e5e0f80795bf282aaf8b5e73cf
Kwame <kwameagyenimboateng64@gmail.com>
2021-09-11 00:31:33 +0000

    detailedVC changes

--- VR_App/controller/Main/DetailedTranscriptionViewController.swift
+++ VR_App/controller/Main/DetailedTranscriptionViewController.swift
@@ -9,12 +9,21 @@ import UIKit
 
 class DetailedTranscriptionViewController: UIViewController {
 
+//    MARK: - variables
     @IBOutlet var dateLabel: UILabel!
     @IBOutlet var titleTextField: UITextField!
     @IBOutlet var descriptionTextArea: UITextView!
+    
+    var transcripts: Transcriptions!
+    var delegate: TranscriptionListDelegate?
+    
     var noteDate: String = ""
     var noteTitle:String = ""
     var noteDescription:String = ""
+    
+    override func viewDidAppear(_ animated: Bool) {
+        titleTextField.becomeFirstResponder()
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         title = "Files"
@@ -22,5 +31,22 @@ class DetailedTranscriptionViewController: UIViewController {
         titleTextField.text = noteTitle
         descriptionTextArea.text = noteDescription
     }
-
+    
+    func updateTranscriptions(){
+        print("updating note")
+        transcripts.date = Date()
+        CoreDataManager.shared.saveTranscriptions()
+        delegate?.refreshTranscriptions()
+    }
+    
+}
+extension DetailedTranscriptionViewController: UITextViewDelegate,UITextFieldDelegate{
+    func textViewDidEndEditing(_ textView: UITextView) {
+        transcripts?.text = descriptionTextArea.text
+        updateTranscriptions()
+    }
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        transcripts?.title = titleTextField.text
+        updateTranscriptions()
+    }
 }
