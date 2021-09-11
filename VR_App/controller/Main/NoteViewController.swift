6c94cae14558ec62a5eec537c2cafc1abdf2095e
Kwame <kwameagyenimboateng64@gmail.com>
2021-09-11 00:32:39 +0000

    noteVC changes

--- VR_App/controller/Main/NoteViewController.swift
+++ VR_App/controller/Main/NoteViewController.swift
@@ -12,20 +12,26 @@ class NoteViewController: UIViewController {
     @IBOutlet weak var dateLabel: UILabel!
     @IBOutlet weak var topicTextField: UITextField!
     @IBOutlet weak var noteTextField: UITextView!
+    
+    var delegate: TranscriptionListDelegate?
     override func viewDidLoad() {
         super.viewDidLoad()
         currentDate()
         hideBottomBar()
+        topicTextField.becomeFirstResponder()
     }
     
 
     @IBAction func onTapClose(_ sender: Any) {
         if topicTextField.text?.isEmpty == false || noteTextField.text.isEmpty == false{
-            showAlert(msg: "You've made changes. Do you want to save them before leaving this page?")
+            showCancelAlert(msg: "You've made changes. Do you want to cancel transcription note?")
         }else{
             dismiss(animated: true, completion: nil)
         }
        
+    }
+    @IBAction func onTapSave(_ sender: Any) {
+        saveRecordingAlert(msg: "Do you want to save?")
     }
 }
 extension NoteViewController{
@@ -47,13 +53,33 @@ extension NoteViewController{
         navigationController?.navigationBar.shadowImage = UIImage()
         navigationController?.navigationBar.isTranslucent = true
     }
-//    show alert
-    func showAlert(msg:String){
-        let alert = UIAlertController(title: "Save changes", message: msg, preferredStyle: .alert)
-        alert.addAction(UIAlertAction(title: "Yes", style: .destructive, handler: { _ in
+//    save notes
+    private func createTranscription() -> Transcriptions{
+        let note = CoreDataManager.shared.createTranscription(title: topicTextField.text ?? "Untitled transcription", text: noteTextField.text ?? "")
+        delegate?.refreshTranscriptions()
+        return note
+    }
+
+//    show cancel alert
+    func showCancelAlert(msg:String){
+        let alert = UIAlertController(title: "Cancel transcription note", message: msg, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "Yes", style: .cancel, handler: { _ in
+            self.dismiss(animated: true, completion: nil)
+        }))
+        alert.addAction(UIAlertAction(title: "No", style: .destructive,handler: { _ in
             
         }))
-        alert.addAction(UIAlertAction(title: "Don't save", style: .cancel,handler: { _ in
+        present(alert, animated: true, completion: nil)
+    }
+//    show save note alert
+    func saveRecordingAlert(msg:String){
+        let alert = UIAlertController(title: "Save changes", message: msg, preferredStyle: .actionSheet)
+        alert.addAction(UIAlertAction(title: "Yes", style: .destructive, handler: { _ in
+//            save transcription here.
+            self.createTranscription()
+            self.dismiss(animated: true, completion: nil)
+        }))
+        alert.addAction(UIAlertAction(title: "No", style: .cancel,handler: { _ in
             self.dismiss(animated: true, completion: nil)
         }))
         present(alert, animated: true, completion: nil)
