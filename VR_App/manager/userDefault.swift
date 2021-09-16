71c54fc9440efe86a6a3e86217256357b151a5b8
Kwame <kwameagyenimboateng64@gmail.com>
2021-09-16 13:48:34 +0000

    userDefaults

--- VR_App/manager/userDefault.swift
+++ VR_App/manager/userDefault.swift
@@ -10,5 +10,16 @@ import UIKit
 
 struct userDefaultManager {
     static let userDefault = UserDefaults.standard
+    static let getData = userDefault.object(forKey: "user") as? [String:String]
+    static let removeData: Void = userDefault.removeObject(forKey: "user")
+    
+    
+    struct userDetails{
+        static let name = getData?["username"] ?? ""
+        static let gender = getData?["gender"] ?? ""
+        static let uuid = getData?["uuid"] ?? ""
+        static let email = getData?["email"] ?? ""
+        static let identity = getData?["identity"] ?? ""
+    }
     
 }
