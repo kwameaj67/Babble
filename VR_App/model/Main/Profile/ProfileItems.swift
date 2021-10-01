36c6f25829b5f4c4776a686190cc2ea54d112065
Kwame <kwameagyenimboateng64@gmail.com>
2021-10-01 09:56:20 +0000

    added more profile sections

--- VR_App/model/Main/Profile/ProfileItems.swift
+++ VR_App/model/Main/Profile/ProfileItems.swift
@@ -7,12 +7,29 @@
 
 import Foundation
 
+
+
 struct ProfileItems{
     
-    let items: [Profile] = [
+    static let section1: [Profile] = [
+        Profile(profileOptions: .viewProfile),
         Profile(profileOptions: .settings),
         Profile(profileOptions: .help),
         Profile(profileOptions: .support),
         Profile(profileOptions: .invite),
     ]
+    
+    static let section2: [Profile] = [
+        Profile(profileOptions: .terms),
+        Profile(profileOptions: .privacy),
+        Profile(profileOptions: .report),
+    ]
+    
+    static let section3: [Profile] = [
+        Profile(profileOptions: .faq),
+        Profile(profileOptions: .contact),
+    ]
+    
+    
+   
 }
