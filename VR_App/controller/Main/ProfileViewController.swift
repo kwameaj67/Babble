005f9135d6199f761be2d38b89d296491205c5b5
Kwame <kwameagyenimboateng64@gmail.com>
2021-10-01 09:58:08 +0000

    add headers to profileTableViews

--- VR_App/controller/Main/ProfileViewController.swift
+++ VR_App/controller/Main/ProfileViewController.swift
@@ -23,8 +23,8 @@ class ProfileViewController: UIViewController {
         profileTableView.delegate = self
         profileTableView.dataSource = self
         profileTableView.tableFooterView = UIView()
-        profileTableView.layer.cornerRadius = 20
-        profileTableView.separatorStyle = .none
+        profileTableView.layer.cornerRadius = 10
+//        profileTableView.separatorStyle = .none
         profileTableView.showsVerticalScrollIndicator = false
         profileTableView.allowsMultipleSelection = false
         profileTableView.backgroundColor = .clear
@@ -49,34 +49,52 @@ class ProfileViewController: UIViewController {
         
         }
     }
-    
-
 }
 extension ProfileViewController: UITableViewDelegate,UITableViewDataSource{
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return Section.sectionArray.count
+    }
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return Section.sectionArray[section].sectionName.uppercased()
+    }
+    
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        let view = UIView(frame: CGRect(x: 0, y: 40, width: tableView.frame.width, height: 60))
+        view.backgroundColor = UIColor(red: 0.96, green: 0.96, blue: 0.96, alpha: 1.00)
+        let label = UILabel(frame: CGRect(x: 0, y: -5, width: Int(view.frame.width), height: 40))
+        label.font = UIFont(name: "Avenir Heavy", size: 12)
+        label.textColor = UIColor(red: 0.49, green: 0.49, blue: 0.49, alpha: 1.00)
+        label.text = Section.sectionArray[section].sectionName.uppercased()
+        view.addSubview(label)
+        return view
+    }
+    func tableView(_ tableView: UITableView, estimatedHeightForHeaderInSection section: Int) -> CGFloat {
+        return 50
+    }
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return profileItems.items.count
+        return Section.sectionArray[section].sectionItems.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         guard let cell = tableView.dequeueReusableCell(withIdentifier: Constants.TableViewCell.profile) as? ProfileTableViewCell else {
             fatalError("ProfileTableViewCell not found")
         }
-        let item = profileItems.items[indexPath.row]
+        let item = Section.sectionArray[indexPath.section].sectionItems[indexPath.row]
         cell.setupProfile(with: item)
         cell.accessoryType = .disclosureIndicator
         cell.selectionStyle = .none
-        
         return cell
     }
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        let item = profileItems.items[indexPath.row]
+        let item = Section.sectionArray[indexPath.section].sectionItems[indexPath.row]
         print("\(item.profileOptions.rawValue)")
+        if indexPath.row == 0{
+//            go to profile screen
+        }
     }
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return 70
+        return 60
     }
-    
-    
 }
 
 extension ProfileViewController{
