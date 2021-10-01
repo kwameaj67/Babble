ee76c743a85350d535e479f18a12553e6a4b07ec
Kwame <kwameagyenimboateng64@gmail.com>
2021-10-01 09:56:40 +0000

    defined profileOptions

--- VR_App/model/Main/Profile/ProfileOptions.swift
+++ VR_App/model/Main/Profile/ProfileOptions.swift
@@ -8,8 +8,16 @@
 import Foundation
 
 enum ProfileOptions: String, CaseIterable{
+    case viewProfile = "View profile"
     case settings = "Settings"
     case help = "Help Center"
     case support = "Support"
     case invite = "Invite Friends"
+    
+    case terms = "Terms and conditions"
+    case privacy = "Privacy Policy"
+    case report = "Report a problem"
+    
+    case faq = "FAQs"
+    case contact = "Contact Us"
 }
