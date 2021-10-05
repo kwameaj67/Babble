f821a2bf786d333388206f60c8a3362cf283b861
Kwame <kwameagyenimboateng64@gmail.com>
2021-10-05 11:54:11 +0000

    modified Actor model

--- VR_App/model/Onboarding/Actor.swift
+++ VR_App/model/Onboarding/Actor.swift
@@ -7,11 +7,23 @@
 
 import Foundation
 
+enum ActorEnum: String, CaseIterable{
+    case name = "Full name"
+    case email = "Email Address"
+    case gender = "Gender"
+    case hearingProfile = "Hearing Profile"
+}
 struct Actor {
-    var email:String
-    var gender:String
-    var identity:String
-    var username:String
-    var uid:String
+    var email:String?
+    var gender:String?
+    var identity:String?
+    var username:String?
+    var uid:String?
+    
+
 }
 
+struct ActorDetails {
+    var title:String
+    var desc:String?
+}
